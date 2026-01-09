2328e2cb3643708048b197b9ea25c3ab604d6c21
P.M.S.V. Jinadasa <sanodyaj@gmail.com>
2026-01-08 21:01:00 -0800

    edit actions

---
+++ editor_actions.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+def move_image(image, x, y):
+    image["x"] = x
+    image["y"] = y
+
+def resize_image(image, scale):
+    image["scale"] *= scale
+
+def rotate_image(image, angle):
+    image["angle"] += angle
