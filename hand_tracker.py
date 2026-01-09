b56f441d1a9d9e77bd45620037255d65d6b0ef49
P.M.S.V. Jinadasa <sanodyaj@gmail.com>
2026-01-08 20:58:37 -0800

    hand_tracker

---
+++ hand_tracker.py
@@ -0,0 +1,17 @@
+import cv2
+import mediapipe as mp
+
+class HandTracker:
+    def __init__(self):
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(max_num_hands=1)
+        self.drawer = mp.solutions.drawing_utils
+
+    def get_landmarks(self, frame):
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        result = self.hands.process(rgb)
+        if result.multi_hand_landmarks:
+            hand = result.multi_hand_landmarks[0]
+            landmarks = [(int(l.x * frame.shape[1]), int(l.y * frame.shape[0])) for l in hand.landmark]
+            return landmarks
+        return None
