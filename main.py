a979edcb2c15fa928f36d454f631c52c3fee53de
P.M.S.V. Jinadasa <sanodyaj@gmail.com>
2026-01-11 07:28:19 -0800

    1

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 
-# -------------------- MODULES --------------------
+
 hand = HandTracker()
 images = ImageManager()
 undo = UndoManager()
