adb86d63c2a0674287058cdbcc5f2115e507ba05
P.M.S.V. Jinadasa <sanodyaj@gmail.com>
2026-01-12 04:56:54 -0800

    1

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ while True:
         if landmarks:
             # Draw a circle on index finger tip 
             cv2.circle(frame, (landmarks[8][0], landmarks[8][1]), 10, (0, 255, 0), -1)
-            # Draw a circle on thumb tip (landmark 4)
+            # Draw a circle on thumb tip 
             cv2.circle(frame, (landmarks[4][0], landmarks[4][1]), 10, (0, 0, 255), -1)
 
 
